d56ef4cad767cb8c6e058eb8945a2259a1139842
DESKTOP-E66TG6M\Nikolic <djordjenikolic001@gmail.com>
2023-11-03 09:42:54 +0100

    yaml and some code for lambda

--- service/classObject/seatInfo.ts
+++ service/classObject/seatInfo.ts
@@ -1,15 +1,11 @@
-
 export class SeatInfo {
-    
   public divs!: Array<string>;
   public idUser!: number;
   public idFlight!: number;
 
-
-  public constructor(divs: Array<string>, idUser: number, idFlight: number){
+  public constructor(divs: Array<string>, idUser: number, idFlight: number) {
     this.divs = divs;
     this.idUser = idUser;
     this.idFlight = idFlight;
   }
-
 }
